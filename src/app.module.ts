8441dea4090c6c367e2de3f4adc29e20f03cdf3e
seungo <ilkhso@gmail.com>
2022-02-17 23:19:16 +0900

    <Feature> Create app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
+import { MoviesModule } from './movies/movies.module';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
-  controllers: [MoviesController], // url을 가져오고 함수를 실행하는 역할 (express의 router)
-  providers: [MoviesService],
+  imports: [MoviesModule],
+  controllers: [AppController],
+  providers: [],
 })
 export class AppModule {}
